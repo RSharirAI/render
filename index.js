cdb688c55db33b15f10ab424d5eeea2e584a94df
RSharirAI <Sharir_learning@proton.me>
2025-07-20 15:56:53 +0300

    Part 3b

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use (express.json())
-
-//**Middleware function */
-app.use(morgan('tiny'))
 app.use(express.static('dist'))
+app.use (express.json())
+app.use(morgan('tiny'))
 
-app.get('/', (request, response) => {
-    console.log('connection set')
-})
+
+
 app.get('/api/notes', (request, response) => {
     Note.find({})
     .then(notes => {
@@ -23,18 +20,16 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findByIdAndDelete(request.params.id)
-    // const id = request.params.id
-    // notes = notes.filter(note => note.id !== id)
-
-    // response.status(204).end()
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request,response) => {
@@ -51,9 +46,39 @@ app.post('/api/notes', (request,response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
     })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if (!note) {
+                return response.status(404).end()
+            }
+        
+
+        note.content = content
+        note.important = important
+        
+        return note.save().then((updatedNote => {
+            response.json(updatedNote)
+        }))
+        })
+        .catch(error => next(error))
 })
 
 //** Middleware function */
@@ -63,6 +88,17 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
